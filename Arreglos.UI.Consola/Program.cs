b68469413eee124535e6ad2550da98a58b0a9ae5
25030018-cripto <25030018@itesa.edu.mx>
2026-09-17 14:51:05 -0600

    Version_02Metodo llenar

---
+++ Arreglos.UI.Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿using Arreglos.Logica;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Operaciones de pila");
+        Console.WriteLine("Arreglo");
+        MiArreglo oMiArreglo = new MiArreglo(100);
+        //oMiArreglo.Llenar(1, 20);
+
+        Console.WriteLine(oMiArreglo);
+        Console.ReadKey();
+    }
+}
